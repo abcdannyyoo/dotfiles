52a09a56ecc4e66c4844fc820d3e86af351fd59d
Danny Yoo <dannyoojh+dev@gmail.com>
2025-08-05 21:45:50 +1200

    update nvim config

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -25,5 +25,23 @@ return {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {},
-  }
+  },
+  {
+    "github/copilot.vim"
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+    keys = {
+      { "<leader>cc", "<cmd>CopilotChatToggle<cr>", desc = "Toggle Copilot Chat" },
+    },
+  },
 }
